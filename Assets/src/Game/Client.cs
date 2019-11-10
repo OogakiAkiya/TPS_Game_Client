a16f9f8d38305e380da3cf35c00f180163ffa2d3
Akiya <kd1220691@st.kobedenshi.ac.jp>
2019-11-11 08:54:36 +0900

    Maynardモデルの追加とアニメーション定数の追加

--- Assets/src/Game/Client.cs
+++ Assets/src/Game/Client.cs
@@ -24,8 +24,10 @@ public class Client : MonoBehaviour
         JumpStay,
         JumpDown,
         Reloading,
+        Attack,
         Dying
     }
+
     private UserBodyData bodyData = new UserBodyData();
 
     public string userID;
