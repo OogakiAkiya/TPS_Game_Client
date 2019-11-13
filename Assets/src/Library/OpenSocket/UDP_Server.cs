325988ae88daeeeb637fa61d49a9e1a9998efd69
Akiya <kd1220691@st.kobedenshi.ac.jp>
2019-11-13 10:42:35 +0900

    共通コンポーネントの更新

--- Assets/src/Library/OpenSocket/UDP_Server.cs
+++ Assets/src/Library/OpenSocket/UDP_Server.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading.Tasks;
 
-class ServerState
+public class ServerState
 {
     public UdpClient socket = null;
     public IPEndPoint endPoint;
@@ -50,49 +49,118 @@ class ServerState
         }
         return count;
     }
+    public void CallBack(IAsyncResult ar)
+    {
+        byte[] buf = socket.EndReceive(ar, ref endPoint);
+        AddRecvData(endPoint, buf);
+    }
 }
 
 
-class UDP_Server
+public class UDP_Server
 {
     public ServerState server { get; private set; } = new ServerState();
-    private ClientState sender = new ClientState();
-    uint sequence = 0;
+    public uint sequence { get; private set; } = 0;
     int port = 0;
-    int sendPort = 12343;
 
     public void Init(int _port)
     {
         port = _port;
         server.socket = new UdpClient(port);
         server.socket.BeginReceive(new AsyncCallback(ReceiveCallback), server);
-        sender.socket = new UdpClient();
 
     }
 
-    public void Init(int _port, int _sendPort)
+
+    public void Send(KeyValuePair<string, int> _address, byte[] _data)
     {
-        port = _port;
-        sendPort = _sendPort;
-        server.socket = new UdpClient(port);
-        server.socket.BeginReceive(new AsyncCallback(ReceiveCallback), server);
-        sender.socket = new UdpClient();
+        byte[] encodeData = CompressionWrapper.Encode(_data);
+        server.socket.SendAsync(encodeData, encodeData.Length, _address.Key, _address.Value);
+        CountUPSequence();
 
     }
-
-
-    public void Update()
+    //本来ならsendPortはportに変わる
+    /*
+    public void AllClientSend(List<string> _ipList, List<byte[]> _data)
     {
+        List<byte> sendDataList = new List<byte>();
+        byte[] sequenceByte = BitConverter.GetBytes(sequence);
+        for (int i = 0; i < _data.Count; i++)
+        {
+            sendDataList.AddRange(BitConverter.GetBytes((_data[i].Length + sequenceByte.Length+sizeof(int))));
+            sendDataList.AddRange(sequenceByte);
+            sendDataList.AddRange(_data[i]);
+        }
+        byte[] encodeData=CompressionWrapper.Encode(sendDataList.ToArray());
+        for (int ip = 0; ip < _ipList.Count; ip++)
+        {
+            server.socket.SendAsync(encodeData,encodeData.Length, _ipList[ip], sendPort);
+        }
+        CountUPSequence();
+
+    }
+    */
+    public void AllClientSend(List<KeyValuePair<string, int>> _address, List<byte[]> _data)
+    {
+        List<byte> sendDataList = new List<byte>();
+        byte[] sequenceByte = BitConverter.GetBytes(sequence);
+        for (int i = 0; i < _data.Count; i++)
+        {
+            sendDataList.AddRange(BitConverter.GetBytes((_data[i].Length + sequenceByte.Length + sizeof(int))));
+            sendDataList.AddRange(sequenceByte);
+            sendDataList.AddRange(_data[i]);
+        }
+        byte[] encodeData = CompressionWrapper.Encode(sendDataList.ToArray());
+        for (int i = 0; i < _address.Count; i++)
+        {
+            server.socket.SendAsync(encodeData, encodeData.Length, _address[i].Key, _address[i].Value);
+            FileController.GetInstance().Write("SendData", "IP:" + _address[i].Key + ",Port:" + _address[i].Value);
+        }
+        CountUPSequence();
+
     }
 
+    public byte[] EncodeData(byte[] _data)
+    {
+        List<byte> data = new List<byte>();
+        byte[] sequenceByte = BitConverter.GetBytes(sequence);
+        data.AddRange(BitConverter.GetBytes((_data.Length + sequenceByte.Length + sizeof(int))));
+        data.AddRange(sequenceByte);
+        data.AddRange(_data);
+        return data.ToArray();
+    }
 
+    //使ってない
+    /*
+    public void AllClientSend(IDictionary<string,int> _iplist, List<byte[]> _data)
+    {
+        byte[][] sendDataArray = new byte[_data.Count][];
+        byte[] head = BitConverter.GetBytes(sequence);
+        for (int i = 0; i < _data.Count; i++)
+        {
+            byte[] addData = new byte[head.Length + _data[i].Length];
+            Array.Copy(head, addData, head.Length);
+            Array.Copy(_data[i], 0, addData, head.Length, _data[i].Length);
+            sendDataArray[i] = addData;
+        }
+        foreach (KeyValuePair<string, int> pair in _iplist)
+        {
+            for (int i = 0; i < _data.Count; i++)
+            {
+                sender.socket.SendAsync(sendDataArray[i], sendDataArray[i].Length, pair.Key, pair.Value);
+                FileController.GetInstance().Write("UDPSend", "IP=" + pair.Key + ",port=" + pair.Value);
+            }
+        }
+        CountUPSequence();
+    }
     //本来ならsendPortはportに変わる
     public void Send(KeyValuePair<IPEndPoint, byte[]> _data)
     {
-        List<byte> sendData=new List<byte>();
+        List<byte> sendData = new List<byte>();
         sendData.AddRange(BitConverter.GetBytes(sequence));
         sendData.AddRange(_data.Value);
         sender.socket.SendAsync(sendData.ToArray(), sendData.ToArray().Length, _data.Key.Address.ToString(), sendPort);
+
         CountUPSequence();
 
     }
@@ -104,28 +172,14 @@ class UDP_Server
         sendData.AddRange(BitConverter.GetBytes(sequence));
         sendData.AddRange(_data);
         sender.socket.SendAsync(sendData.ToArray(), sendData.ToArray().Length, _IP, _port);
+
         CountUPSequence();
 
     }
+    */
 
-    //本来ならsendPortはportに変わる
-    public void AllClietnSend(List<string> _ipList, List<byte[]> _data)
-    {
-        foreach (string IP in _ipList)
-        {
-            foreach (byte[] data in _data)
-            {
-                List<byte> sendData = new List<byte>();
-                sendData.AddRange(BitConverter.GetBytes(sequence));
-                sendData.AddRange(data);
 
-                sender.socket.SendAsync(sendData.ToArray(), sendData.ToArray().Length, IP, sendPort);
-            }
-        }
-        CountUPSequence();
-    }
-
-    private void CountUPSequence()
+    public void CountUPSequence()
     {
         sequence++;
         if (sequence > 4200000000)
@@ -137,9 +191,14 @@ class UDP_Server
     private void ReceiveCallback(IAsyncResult ar)
     {
         ServerState client = (ServerState)ar.AsyncState;
+        //client.CallBack(ar);
+        //client.socket.BeginReceive(ReceiveCallback, client);
+
+
         byte[] buf = client.socket.EndReceive(ar, ref client.endPoint);
         client.AddRecvData(client.endPoint, buf);
         client.socket.BeginReceive(ReceiveCallback, client);
+
     }
 
 }
